5a2870b7d0053496e973597ecd00b5baa61b2e2c
Viacheslav Pleshkov <mail@viacheslavpleshkov.com>
2020-07-29 20:42:20 +0300

    Update telescope

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,4 +27,16 @@ class AuthServiceProvider extends ServiceProvider
 
         //
     }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->isLocal()) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
+    }
 }
