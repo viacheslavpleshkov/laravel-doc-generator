82456dd744579969fb2ba615002dfbf41b4d1bfe
Viacheslav Pleshkov <mail@viacheslavpleshkov.com>
2020-08-15 17:09:58 +0300

    Update html situatiuns

--- resources/views/site/pages/types.blade.php
+++ resources/views/site/pages/types.blade.php
@@ -4,19 +4,11 @@
 @section('content')
     <div class="card-deck mb-3 text-center">
         @foreach($situation as $item)
-            <div class="card mb-4 shadow-sm">
+            <a href="{{ route('site.situation.index', $item->id) }}" class="card mb-4 shadow-sm">
                 <div class="card-header head-card-bs">
-                    <h4 class="my-0 font-weight-normal text-card-bs">{{ $item->name }}</h4>
+                    <h4 class="my-0 font-weight-normal text-card-bs text-dark">{{ $item->name }}</h4>
                 </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">{{ $item->price }} рубль</h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li>{{ $item->description }}</li>
-                    </ul>
-                    <a href="{{ route('site.situation.index', $item->id) }}"
-                       class="btn btn-lg btn-block btn-outline-secondary">Создать документ</a>
-                </div>
-            </div>
+            </a>
         @endforeach
     </div>
 @endsection
